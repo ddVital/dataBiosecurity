ca225da0c48d47e4a615d11ef13a732cdf0c6cdc
David <ddvital0@gmail.com>
2024-05-05 18:53:31 -0300

    fixing validations

--- biosecurity/settings.py
+++ biosecurity/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c)ax&9aars$hlc2k*=lf(j$_y!ep4p_sv&l_xb0-6&vo_b5kvu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
